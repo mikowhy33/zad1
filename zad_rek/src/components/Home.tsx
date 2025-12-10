d62438b3695fe8a7d7f7c59eae02d30f8dcb6875
Mikolaj Babicz <koding252@gmail.com>
2025-12-10 12:00:55 +0100

    [feat] - after the user added on main page the item, it statys - tracking added products by displaying the number added to the cart

--- zad_rek/src/components/Home.tsx
+++ zad_rek/src/components/Home.tsx
@@ -13,21 +13,26 @@ export function Home({ addToCart }: AddToCart) {
   console.log(dataFromApi);
 
   console.log(dataFromApi.length + 'Length');
-  const randomNr = Math.floor(Math.random() * 20);
 
-  console.log(randomNr);
+  // console.log(randomNr);
 
-  
+  const [timesAddesToTheBasket, setTimesAddesToTheBasket] = useState(0);
 
-  const [addedRandom, setAddedRandom] = useState(false);
+  const randomItem = useMemo(() => {
+    const randomNr = Math.floor(Math.random() * 20);
+
+    return dataFromApi[randomNr];
+  }, [dataFromApi]);
 
   const AddedToTheBasket = () => {
     // setTimeout(() => {
     setAddedRandom(true);
     // }, 1000);
 
-    addToCart(dataFromApi[randomNr]);
+    addToCart(randomItem);
+    setTimesAddesToTheBasket((prev) => prev + 1);
   };
+  const [addedRandom, setAddedRandom] = useState(false);
 
   if (loading) {
     return <>Loading the products</>;
@@ -37,21 +42,25 @@ export function Home({ addToCart }: AddToCart) {
     return <>There has been an error while fetching the data</>;
   }
 
-  if (addedRandom) {
-    return <p>Added to the basket</p>;
-  }
+  // if (addedRandom) {
+  //   return <p>Added to the basket</p>;
+  // }
 
   return (
-    <>
+    <div className="flex flex-col gap-y-4">
       <div>This is a HomePage</div>
 
       <div className="w-24 h-24 mx-auto">
-        <img src={dataFromApi[randomNr].image} alt="" className="w-full h-full" />
+        <img src={randomItem.image} alt="" className="w-full h-full" />
       </div>
 
-      <button className="w-fit" onClick={AddedToTheBasket}>
+      <button className="w-fit mx-auto" onClick={AddedToTheBasket}>
         Add to the basket
       </button>
-    </>
+
+      {addedRandom === true ? (
+        <p className="bg-green-300 w-xs mx-auto text-black rounded-2xl">Added {timesAddesToTheBasket} an item to the basket!</p>
+      ) : null}
+    </div>
   );
 }
--- zad_rek/src/main.tsx
+++ zad_rek/src/main.tsx
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
-
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </StrictMode>,
+    //{' '}
+  </StrictMode>,
 );
